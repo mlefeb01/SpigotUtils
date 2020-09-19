da761826af7052b898e597219535c62e12ffab24
Matt Lefebvre <matthewelefebvre@gmail.com>
2020-09-19 16:41:49 -0400

    Add methods to fully repair items, repair a percentage of an items missing durability, and set an items durability to some percentage of its max durability

--- spigotutils-api/src/main/java/com/github/mlefeb01/spigotutils/api/utils/ItemUtils.java
+++ spigotutils-api/src/main/java/com/github/mlefeb01/spigotutils/api/utils/ItemUtils.java
@@ -1,5 +1,6 @@
 package com.github.mlefeb01.spigotutils.api.utils;
 
+import com.github.mlefeb01.spigotutils.api.constants.MaterialConstants;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
@@ -95,6 +96,50 @@ public final class ItemUtils {
         } else {
             return (r < 40) ? (double) fortuneLevel - 3 : (r < 60) ? (double) fortuneLevel - 2 : (r < 80) ? (double) fortuneLevel - 1 : (double) fortuneLevel;
         }
+    }
+
+    /**
+     * Fully repairs an item if the item is repairable and has missing durability
+     *
+     * @param item item
+     * @return if the item was able to be repaired
+     */
+    public static boolean repairItem(ItemStack item) {
+        return repairItem(item, 100);
+    }
+
+    /**
+     * Restores a percentage of an items missing durability if the item is both repairable and has missing durability
+     * precondition: percent is greater than 0 and less than or equal to 100
+     *
+     * @param item item
+     * @param percent the % of missing durability to repair
+     * @return if the item was able to be repaired
+     */
+    public static boolean repairItem(ItemStack item, double percent) {
+        if (percent > 100 || percent <= 0 || ItemUtils.isNullOrAir(item) || !MaterialConstants.isRepairable(item.getType()) || item.getDurability() == 0) {
+            return false;
+        }
+
+        item.setDurability((short) (item.getDurability() - (item.getDurability() * (percent/100))));
+        return true;
+    }
+
+    /**
+     * Sets an items durability to a % of its max if it is repairable
+     * precondition: percent is greater than 0 and less than or equal to 100
+     *
+     * @param item item
+     * @param percent the percentage of the items max durability to set its current durability to
+     * @return if the item's durability was successfully updated
+     */
+    public static boolean setItemDurability(ItemStack item, double percent) {
+        if (percent <= 0 || percent > 100 || ItemUtils.isNullOrAir(item) || !MaterialConstants.isRepairable(item.getType())) {
+            return false;
+        }
+
+        item.setDurability((short) (item.getType().getMaxDurability() * ((100 - percent)/100)));
+        return true;
     }
 
 }
