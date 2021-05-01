d7929b53c4fc3a3c7ce6ee7f0f02cd11246b58ad
Matt Lefebvre <matthewelefebvre@gmail.com>
2021-05-01 00:12:31 -0400

    Paginate sub commands in the help message, remove unnecessary final modifiers on methods, and expose parent reference to subclasses

---
+++ spigotutils-api/src/main/java/com/github/mlefeb01/spigotutils/api/command/AbstractCmdHelp.java
@@ -0,0 +1,31 @@
+package com.github.mlefeb01.spigotutils.api.command;
+
+import com.github.mlefeb01.spigotutils.api.command.parametertypes.TypeInteger;
+
+/**
+ * Sends sub-commands and their descriptions for a parent command
+ * @author Matt Lefebvre
+ */
+public abstract class AbstractCmdHelp extends AbstractCommand {
+
+    public AbstractCmdHelp() {
+        super("help");
+        addParameter(TypeInteger.getInstance(), "page", 1);
+        setDescription("Sends the help command");
+    }
+
+    @Override
+    public void execute() throws Exception {
+        // Make the page parameter function like an optional parameter
+        int page = 1;
+        if (rawArgs.length > 0) {
+            try {
+                page = Integer.parseInt(rawArgs[1]);
+            } catch (Exception e) {
+                // Ignore
+            }
+        }
+        sender.sendMessage(parent.generateHelpCommand(page));
+    }
+
+}
